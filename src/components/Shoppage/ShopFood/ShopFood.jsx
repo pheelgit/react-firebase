45f64e50210864ec6125921f193f7f08e593fc09
pheelgit <philatov63@mail.ru>
2022-10-07 06:58:44 +0300

    add db shop items

--- src/components/Shoppage/ShopFood/ShopFood.jsx
+++ src/components/Shoppage/ShopFood/ShopFood.jsx
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import { db } from "firebase.js";
+import { onValue, ref } from "firebase/database";
+import { useState } from "react";
 
 export const ShopFood = () => {
-	return <div>ShopFood</div>;
+	const [items, setItems] = useState([]);
+
+	useEffect(() => {
+		onValue(ref(db, "shoplist/food"), (snapshot) => {
+			const data = snapshot.val();
+			data && setItems(Object.values(data));
+		});
+	}, []);
+
+	return <div>{JSON.stringify(items)}</div>;
 };
--- src/components/Shoppage/ShopOther/ShopOther.jsx
+++ src/components/Shoppage/ShopOther/ShopOther.jsx
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import { db } from "firebase.js";
+import { onValue, ref } from "firebase/database";
+import { useState } from "react";
 
 export const ShopOther = () => {
-	return <div>ShopOther</div>;
+	const [items, setItems] = useState([]);
+
+	useEffect(() => {
+		onValue(ref(db, "shoplist/other"), (snapshot) => {
+			const data = snapshot.val();
+			data && setItems(Object.values(data));
+		});
+	}, []);
+
+	return <div>{JSON.stringify(items)}</div>;
 };
